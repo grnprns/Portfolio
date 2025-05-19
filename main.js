7cc51b68bd0ce27aa13578f009aa99c5d927f079
grnprns <164390543+grnprns@users.noreply.github.com>
2025-05-19 16:31:26 +0200

    finished header

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-const siteHeader = document.getElementsByTagName("site-header")[0];
-const scrollWatcher = document.createElement("div");
-console.log(siteHeader);
+// const siteHeader = document.getElementsByTagName("site-header")[0];
+// const scrollWatcher = document.createElement("div");
+// console.log(siteHeader);
 
-scrollWatcher.setAttribute("data-scroll-watcher", "");
-siteHeader.before(scrollWatcher);
+// scrollWatcher.setAttribute("data-scroll-watcher", "");
+// siteHeader.before(scrollWatcher);
 
-const navObserver = new IntersectionObserver((entries) => {
-    // 2nd parameter changes how toggle works, if 2nd parameter returns false, toggle can only remove a class and vice versa
-    // https://www.youtube.com/watch?v=V-CBdlfCPic&t=315s
-    siteHeader.classList.toggle("sticking", !entries[0].isIntersecting);
-});
+// const navObserver = new IntersectionObserver((entries) => {
+//     // 2nd parameter changes how toggle works, if 2nd parameter returns false, toggle can only remove a class and vice versa
+//     // https://www.youtube.com/watch?v=V-CBdlfCPic&t=315s
+//     siteHeader.classList.toggle("sticking", !entries[0].isIntersecting);
+// });
 
-navObserver.observe(scrollWatcher);
+// navObserver.observe(scrollWatcher);
+
+
+let headings = document.querySelectorAll("h2, h3");
