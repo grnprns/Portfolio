5e9b95e01f36d06d3aece37e0ab6a01946c6d539
grnprns <164390543+grnprns@users.noreply.github.com>
2025-05-19 16:57:48 +0200

    cleanup

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-// const siteHeader = document.getElementsByTagName("site-header")[0];
-// const scrollWatcher = document.createElement("div");
-// console.log(siteHeader);
-
-// scrollWatcher.setAttribute("data-scroll-watcher", "");
-// siteHeader.before(scrollWatcher);
-
-// const navObserver = new IntersectionObserver((entries) => {
-//     // 2nd parameter changes how toggle works, if 2nd parameter returns false, toggle can only remove a class and vice versa
-//     // https://www.youtube.com/watch?v=V-CBdlfCPic&t=315s
-//     siteHeader.classList.toggle("sticking", !entries[0].isIntersecting);
-// });
-
-// navObserver.observe(scrollWatcher);
-
-
 document.querySelectorAll('#projects a h2, #projects a h3').forEach(el => {
     el.addEventListener('mouseover', () => {
         el.style.removeProperty('color');
